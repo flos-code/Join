d7425eacad93c6ccb5727acd38e5d01101203172
Makedonka Sander <makedonka28@gmail.com>
2023-11-15 15:38:28 +0100

    logout function

--- js/header.js
+++ js/header.js
@@ -14,9 +14,7 @@ function showLogout() {
         </div>
     </div>
     `;
-    helpMenuActive();
-    legalNoticeMenuActive();
-    privacyMenuActive();
+    
 }
 
 
@@ -36,105 +34,5 @@ function doNotClose(event) {
 }
 
 
-/**
-* clear active user status and send back to index.html - log in
-*/
-async function logout() {
-    //await backend.setItem('activeUser', JSON.stringify(activeUser.length = 0));
-    let activeUserJson = JSON.stringify(activeUser);
-    localStorage.setItem('activeUser', activeUserJson);
-    window.open("index.html", "_self");
-}
 
 
-/**
-* open the privacy html
-*/
-function openPrivacy() {
-    window.open("privacy_policy.html", "_self");
-}
-
-
-/**
-* open the help html
-*/
-function openHelp() {
-    window.open("help.html", "_self");
-}
-
-
-/**
-* open the legal notice html
-*/
-function openLegalNotice() {
-    window.open("legal_notice.html", "_self");
-}
-
-
-/**
-* Show active site on the navigation bar -> Legal notice
-*/
-function addActiveClass4() {
-    setTimeout(function () {
-        document.getElementById('id-4').classList.add('active');
-    }, 125);
-}
-
-
-/**
-* Show active site on the navigation bar -> Legal notice
-*/
-function addActiveClass5() {
-    setTimeout(function () {
-        document.getElementById('id-5').classList.add('active');
-    }, 125);
-}
-
-
-/**
-* Hide question mark on header bar like in the mockup-> Help
-*/
-function addActiveClass6() {
-    setTimeout(function () {
-        document.getElementById('id-6').classList.add('d-none');
-    }, 125);
-}
-
-
-/**
-* Show active site on the responsive navigation bar -> Help
-*/
-function helpMenuActive() {
-    if (document.getElementById('help-active') == null) {
-    } else {
-        if (document.getElementById('idResponsive-6').innerHTML == document.getElementById('help-active').innerHTML) {
-            document.getElementById('idResponsive-6').classList.add('rs-logout-active');
-        }
-    }
-}
-
-
-/**
-* Show active site on the responsive navigation bar -> Legal notice
-*/
-function legalNoticeMenuActive() {
-    if (document.getElementById('legal-notice-active') == null) {
-    } else {
-        if (document.getElementById('idResponsive-4').innerHTML == document.getElementById('legal-notice-active').innerHTML) {
-            document.getElementById('idResponsive-4').classList.add('rs-logout-active');
-        }
-    }
-}
-
-
-/**
-* Show active site on the responsive navigation bar -> Privacy
-*/
-function privacyMenuActive() {
-    if (document.getElementById('privacy-active') == null) {
-    } else {
-        if (document.getElementById('idResponsive-5').innerHTML == document.getElementById('privacy-active').innerHTML) {
-            document.getElementById('idResponsive-5').classList.add('rs-logout-active');
-        }
-    }
-}
